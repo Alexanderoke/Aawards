18479a8d11e521c1f8549b60864148ec0b32259f
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-16 11:41:39 +0300

    create post se model

--- appward/models.py
+++ appward/models.py
@@ -1,3 +1,12 @@
+from sre_parse import CATEGORIES
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    sitename= models.TextField()
+    URL=models.URLField()
+    Description=models.CharField(max_length=100)
+    categories=models.CharField(max_length=50)
+    technologies= models.CharField()
+    author =models.ForeignKey(User, on_delete=models.CASCADE)
