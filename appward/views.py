07a569094bf5ef25e09ba2803662df11c5181898
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-20 18:01:24 +0300

    try post site picturesfrom admin

--- appward/views.py
+++ appward/views.py
@@ -1,25 +1,21 @@
-from django.shortcuts import render,redirect
-from django.contrib import messages
+from django.shortcuts import render
 from .form import PostForm
 from . models import Post
 
 # Create your views here.
 def home(request):
-  if request.method =='POST':
-    form = PostForm(request.POST)
-    if form.is_valid():
-      form.save()
-      obj=form.instance
-      return render(request,"appward/home.html",{"obj":obj})
-      
-  else:
-    form =PostForm()
+  form = PostForm(request.POST,request.FILES)
+  if form.is_valid():    
+    form.save()
   context ={
     'form':PostForm(),
     'title':'Home',
     'posts':Post.objects.all()
   }
   return render(request, 'appward/home.html', context)
+
+
+  
 
 # if request.method =='POST':
 #     form = UserRegisterForm(request.POST)
