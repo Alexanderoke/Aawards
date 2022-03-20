5f3430a02acfa50fc62cae8f657cbd5cda3ae1ec
alexanderoke <alexander.oke@student.moringaschool.com>
2022-03-20 19:57:43 +0300

    create app api

---
+++ api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    sitename= models.TextField()
+    image= models.ImageField(upload_to='images')
+    URL=models.URLField()
+    Description=models.CharField(max_length=100)
+    categories=models.CharField(max_length=50)
+    technologies= models.CharField(max_length=100)
+    # author =models.ForeignKey(User, on_delete=models.CASCADE)
